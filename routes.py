122fb28aa1af3208a412940c16a8be92675b8448
angeliki-tzanou <angeliki.tzanou@stonybrook.edu>
2023-10-28 00:30:01 +0000

    flask app

--- routes.py
+++ routes.py
@@ -1,9 +1,10 @@
 from flask import render_template
 from sqlalchemy import create_engine, inspect
 
-from app_gcp import app_gcp 
+from app_gcp import app_gcp  # Import the Flask app instance from app_gcp.py
 
-gcp_db_uri = "mysql+pymysql://root:tza504!!@34.170.238.2/ang"
+# Replace with your GCP database connection details
+gcp_db_uri = "mysql+pymysql://username:password@hostname/database_name"
 
 engine = create_engine(gcp_db_uri)
 
@@ -12,6 +13,10 @@ def home():
     insp = inspect(engine)
     table_names = insp.get_table_names()
     return render_template('home.html', tables=table_names)
+
+
+
+
 
 
 
